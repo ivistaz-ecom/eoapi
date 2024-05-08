f7a0903046163c7defd05a4c42dbcb39d67230b5
kallolghosal <kghosal@gmail.com>
2024-05-08 15:06:12 +0530

    Member controller updated

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Backend\MemberController;
 
 /*
 |--------------------------------------------------------------------------
@@ -23,4 +24,4 @@ Auth::routes([
     'verify' => false,
 ]);
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/home', [MemberController::class, 'index'])->name('home');
