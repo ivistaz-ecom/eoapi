fbf2857d86896bbf81f8fd18c9df6c9b4ceee9d0
kallolghosal <kghosal@gmail.com>
2024-06-11 20:03:37 +0530

    Dashboard data changed

--- app/Http/Controllers/Backend/HomeController.php
+++ app/Http/Controllers/Backend/HomeController.php
@@ -38,7 +38,7 @@ class HomeController extends Controller
         // ->leftJoin('eomembers.id', '=', 'paymentinfo.eoid')
         // ->select('eomembers.id', 'eomembers.firstname', 'eomembers.lastname', 'eochapters.chapters', 'eoregions.region', 'paymentinfo.currency', 'paymentinfo.amount', 'paymentinfo.created_at')
         // ->where('paymentinfo.paymentstatus', '=', 'success')->orderBy('eomembers.id', 'DESC')->paginate(10);
-        $regmembers = DB::select(DB::raw("SELECT eomembers.id, eomembers.firstname, eomembers.lastname, eochapters.chapters, eoregions.region, paymentinfo.currency, paymentinfo.amount, paymentinfo.created_at FROM eomembers LEFT JOIN eochapters ON eomembers.chapter = eochapters.id LEFT JOIN eoregions ON eomembers.region = eoregions.id left JOIN paymentinfo ON eomembers.id = paymentinfo.eoid WHERE paymentinfo.paymentstatus = 'success' order by paymentinfo.id DESC"));
+        $regmembers = DB::select("SELECT eomembers.id, eomembers.firstname, eomembers.lastname, eochapters.chapters, eoregions.region, paymentinfo.currency, paymentinfo.amount, paymentinfo.created_at FROM eomembers LEFT JOIN eochapters ON eomembers.chapter = eochapters.id LEFT JOIN eoregions ON eomembers.region = eoregions.id left JOIN paymentinfo ON eomembers.id = paymentinfo.eoid WHERE paymentinfo.paymentstatus = 'success' order by paymentinfo.id DESC")->paginate(10);
         return view('home', ["members" => $members, 'payment' => $payment, 'offers' => $offers, 'regmembers' => $regmembers]);
     }
 }
