7bfd9c82c9f10e4a064a4aa9cda3f996f1971e4c
kallolghosal <kghosal@gmail.com>
2024-06-17 17:40:49 +0530

    Search result page updated

--- resources/views/searchresult.blade.php
+++ resources/views/searchresult.blade.php
@@ -2,60 +2,85 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Member Detail') }}</div>
-                @if ($member === 'none')
-                <div class="card-body">Please search key</div>
-                @elseif ($member->count() == 0)
-                <div class="card-body">No result found</div>
-                @else
-                <div class="card-body">
-                    <table class="table table-striped mt-4">
-                        <tr>
-                            <td>First Name</td>
-                            <td>{{ $member[0]->firstname }}</td>
-                        </tr>
-                        <tr>
-                            <td>Last Name</td>
-                            <td>{{ $member[0]->lastname }}</td>
-                        </tr>
-                        <tr>
-                            <td>Gender</td>
-                            <td>{{ $member[0]->gender }}</td>
-                        </tr>
-                        <tr>
-                            <td>Spouse ID</td>
-                            <td>{{ $member[0]->spouse }}</td>
-                        </tr>
-                        <tr>
-                            <td>Email</td>
-                            <td>{{ $member[0]->email }}</td>
-                        </tr>
-                        <tr>
-                            <td>Chapter</td>
-                            <td>{{ $member[0]->chapters }}</td>
-                        </tr>
-                        <tr>
-                            <td>Region</td>
-                            <td>{{ $member[0]->region }}</td>
-                        </tr>
-                        <tr>
-                            <td>Joining Date</td>
-                            <td>{{ $member[0]->joindt }}</td>
-                        </tr>
-                        <tr>
-                            <td>Industry</td>
-                            <td>{{ $member[0]->industry }}</td>
-                        </tr>
-                    </table>
-                    <a href="{{ route('edit-member', $member[0]->id) }}" class="btn btn-primary">Edit Member</a>&nbsp;
-                    <a href="{{ route('members') }}" class="btn btn-primary">All Members</a>
-                    @endif
-                </div>
+    <div class="card">
+        <div class="card-header">{{ __('Member Detail') }}</div>
+        @if ($member === 'none')
+            <div class="card-body">Please search key</div>
+        @elseif ($member->count() == 0)
+            <div class="card-body">No result found</div>
+        @elseif ($member->count() == 1)
+            <div class="card-body">
+                <table class="table table-striped mt-4">
+                    <tr>
+                        <td>First Name</td>
+                        <td>{{ $member[0]->firstname }}</td>
+                    </tr>
+                    <tr>
+                        <td>Last Name</td>
+                        <td>{{ $member[0]->lastname }}</td>
+                    </tr>
+                    <tr>
+                        <td>Gender</td>
+                        <td>{{ $member[0]->gender }}</td>
+                    </tr>
+                    <tr>
+                        <td>Spouse ID</td>
+                        <td>{{ $member[0]->spouse }}</td>
+                    </tr>
+                    <tr>
+                        <td>Email</td>
+                        <td>{{ $member[0]->email }}</td>
+                    </tr>
+                    <tr>
+                        <td>Chapter</td>
+                        <td>{{ $member[0]->chapter }}</td>
+                    </tr>
+                    <tr>
+                        <td>Region</td>
+                        <td>{{ $member[0]->region }}</td>
+                    </tr>
+                    <tr>
+                        <td>Joining Date</td>
+                        <td>{{ $member[0]->joindt }}</td>
+                    </tr>
+                    <tr>
+                        <td>Industry</td>
+                        <td>{{ $member[0]->industry }}</td>
+                    </tr>
+                </table>
+                <a href="{{ route('edit-member', $member[0]->id) }}" class="btn btn-primary">Edit Member</a>&nbsp;
+                <a href="{{ route('members') }}" class="btn btn-primary">All Members</a>
             </div>
-        </div>
+        @else 
+            <table class="table table-striped mt-4">
+                <thead>
+                    <tr>
+                        <td>SL</td>
+                        <td>First Name</td>
+                        <td>Last Name</td>
+                        <td>Email</td>
+                        <td>Chapter</td>
+                        <td>Region</td>
+                        <td>Spouse ID</td>
+                        <td>Edit</td>
+                    </tr>
+                </thead>
+                @foreach ($member as $mem)
+                <tr>
+                    <td>{{ $mem->id }}</td>
+                    <td>{{ $mem->firstname }}</td>
+                    <td>{{ $mem->lastname }}</td>
+                    <td>{{ $mem->email }}</td>
+                    <td>{{ $mem->chapter }}</td>
+                    <td>{{ $mem->region }}</td>
+                    <td>{{ $mem->spouse_id }}</td>
+                    <td><a href="{{ route('show-member', $mem->id) }}"><i class="bi bi-eye"></i></a>&nbsp;<a href="{{ route('edit-member', $mem->id) }}"><i class="bi bi-pencil"></i></a>&nbsp;<a href="{{ route('delete-member', $mem->id) }}" onclick="return confirm('Are you sure you want to delete this member?');"><i class="bi bi-trash"></i></a></td>
+                </tr>
+                @endforeach
+            </table>
+            {!! $member->withQueryString()->links('pagination::bootstrap-5') !!}
+        
+        @endif
     </div>
 </div>
 @endsection
