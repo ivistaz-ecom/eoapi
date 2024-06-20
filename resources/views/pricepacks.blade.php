7a311395041dd8487563c858823a9cc95ee544d0
kallolghosal <kghosal@gmail.com>
2024-06-20 10:45:41 +0530

    Pricepack page updated

--- resources/views/pricepacks.blade.php
+++ resources/views/pricepacks.blade.php
@@ -8,32 +8,37 @@
             @if (session('message'))
                 <h6 class="text-success">{{ session('message') }}</h6>
             @endif
-            <table class="table table-striped mt-4">
-                <thead>
+
+            @if (count($pricepacks) < 1)
+                <h6>No packs exists</h6>
+            @else 
+                <table class="table table-striped mt-4">
+                    <thead>
+                        <tr>
+                            <td>Package</td>
+                            <td>Order</td>
+                            <td>Member Fee</td>
+                            <td>SLP Fee</td>
+                            <td>Booked</td>
+                            <td>Count</td>
+                            <td>Status</td>
+                            <td>Edit</td>
+                        </tr>
+                    </thead>
+                    @foreach ($pricepacks as $packs)
                     <tr>
-                        <td>Package</td>
-                        <td>Order</td>
-                        <td>Member Fee</td>
-                        <td>SLP Fee</td>
-                        <td>Booked</td>
-                        <td>Count</td>
-                        <td>Status</td>
-                        <td>Edit</td>
+                        <td>{{ $packs->packagename }}</td>
+                        <td>{{ $packs->packageorder }}</td>
+                        <td>{{ $packs->memberfee }}</td>
+                        <td>{{ $packs->slpfee }}</td>
+                        <td>{{ $packs->numbooked }}</td>
+                        <td>{{ $packs->totalcount }}</td>
+                        <td>{{ $packs->offerstatus }}</td>
+                        <td><a href="{{ route('edit-pack', $packs->id) }}"><i class="bi bi-pencil"></i></a>&nbsp;<a href="{{ route('delete-pack', $packs->id) }}" onclick="return confirm('Are you sure you want to delete this member?');"><i class="bi bi-trash"></i></a></td>
                     </tr>
-                </thead>
-                @foreach ($pricepacks as $packs)
-                <tr>
-                    <td>{{ $packs->packagename }}</td>
-                    <td>{{ $packs->packageorder }}</td>
-                    <td>{{ $packs->memberfee }}</td>
-                    <td>{{ $packs->slpfee }}</td>
-                    <td>{{ $packs->numbooked }}</td>
-                    <td>{{ $packs->totalcount }}</td>
-                    <td>{{ $packs->offerstatus }}</td>
-                    <td><a href="{{ route('edit-pack', $packs->id) }}"><i class="bi bi-pencil"></i></a>&nbsp;<a href="{{ route('delete-pack', $packs->id) }}" onclick="return confirm('Are you sure you want to delete this member?');"><i class="bi bi-trash"></i></a></td>
-                </tr>
-                @endforeach
-            </table>
+                    @endforeach
+                </table>
+            @endif
         </div>
     </div>
 </div>
